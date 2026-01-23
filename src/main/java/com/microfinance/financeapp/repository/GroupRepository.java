539808bb88c5362daa3e713d6b347b0330720170
Vaishaklogan <rvaishak35@gmail.com>
2026-01-23 13:00:28 +0530

    Fix GroupRepository for active group filtering

--- src/main/java/com/microfinance/financeapp/repository/GroupRepository.java
+++ src/main/java/com/microfinance/financeapp/repository/GroupRepository.java
@@ -3,5 +3,10 @@ package com.microfinance.financeapp.repository;
 import com.microfinance.financeapp.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    // For soft delete logic
+    List<Group> findByStatus(String status);
 }
