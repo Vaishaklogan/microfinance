e348976418614c33876864f7c2724151aab7f46b
Vaishaklogan <rvaishak35@gmail.com>
2026-01-24 09:34:06 +0530

    fix ambiguous mapping for root path

--- src/main/java/com/microfinance/financeapp/controller/HomeController.java
+++ src/main/java/com/microfinance/financeapp/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.microfinance.financeapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
-        return "dashboard";
+        return "home";
     }
 }
