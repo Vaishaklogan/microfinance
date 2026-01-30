764040f3ad46e27ed535c2b060848cd5bf6a10ce
Vaishaklogan <rvaishak35@gmail.com>
2026-01-30 08:55:33 +0530

    remove unwanted weekly-controller in dashboard controller

--- src/main/java/com/microfinance/financeapp/controller/DashboardController.java
+++ src/main/java/com/microfinance/financeapp/controller/DashboardController.java
@@ -1,38 +1,13 @@
 package com.microfinance.financeapp.controller;
 
-import com.microfinance.financeapp.repository.LoanRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.time.LocalDate;
 
 @Controller
 public class DashboardController {
 
-    private final LoanRepository loanRepository;
-
-    public DashboardController(LoanRepository loanRepository) {
-        this.loanRepository = loanRepository;
-    }
-
     @GetMapping("/")
     public String dashboard() {
         return "dashboard";
-    }
-
-    @GetMapping("/weekly-collection")
-    public String weeklyCollection(Model model) {
-
-        LocalDate today = LocalDate.now();
-
-        model.addAttribute(
-                "loans",
-                loanRepository
-                        .findByStatusAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
-                                "ACTIVE", today, today)
-        );
-
-        return "weekly-collection";
     }
 }
